b5a4ff295c1481cfafa6a652d2e0460174413f7b
mark.costales <mcostales@t2s-solutions.com>
2020-11-06 01:41:43 -0500

    set up map

--- src/components/BrewMap.vue
+++ src/components/BrewMap.vue
@@ -1,16 +1,38 @@
 <template>
-	<div>
-		<h2>map</h2>
+	<div class="row map">
+		<l-map :zoom="zoom" :center="center">
+      <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+      <l-marker :lat-lng="marker"></l-marker>
+    </l-map>
 	</div>
+	<!-- /.row map -->
 </template>
 
 <script>
+import L from 'leaflet';
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+
 export default {
 	name: "BrewMap",
-
+	components: {
+	LMap,
+	LTileLayer,
+	LMarker,
+	},
+	data(){
+		return{
+			zoom:13,
+			center: L.latLng(47.413220, -1.219482),
+			url:'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+			attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+			marker: L.latLng(47.413220, -1.219482),
+			}
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.map{
+	height: 100%;
+}
 </style>
